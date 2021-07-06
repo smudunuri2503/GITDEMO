2bb218c07ce309010edd3e26196f4c21e3ee9e7e
Sindhu Priya M <sindhupriyam@Sindhus-MacBook-Pro.local>
2021-07-07 01:00:11 +0530

    Temp

--- src/test/classtocommit.java
+++ src/test/classtocommit.java
@@ -2,6 +2,8 @@ package test;
 
 public class classtocommit {
 	public static void main(String[] args)
-	{System.out.println("Sindhu");}
+	{System.out.println("Sindhu3");
+	System.out.println("Sindhu2");
+	System.out.println("Sindhu");}
 
 }
